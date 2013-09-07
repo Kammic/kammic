a4aa1b94b28edec55dc55fe19753fa4d07de3dd2
Sumeet Singh <ortuna@gmail.com>
2013-09-07 13:15:16 -0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kammic::Application.config.secret_key_base = '1edb97631b9028543ddcdd8fc65c05155c868100582df2625124b625cea9157ba532809e50f0c8e7b1c50d15cc009b63dd84e1be0409bc532a93b9b1daee0ec5'
