8dc021ce0be68ddf4ca14970c3e59b0daf28a5cc
Sumeet Singh <ortuna@gmail.com>
2013-09-07 14:25:51 -0400

    Added resizer

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,1 +1,31 @@
-//= require_tree ./lib/angular
+var Editor = function() {
+  this.aceEditor = null;
+
+  var $editor    = $('#editor');
+  var $preview   = $('#preview');
+  var $container = $('#container');
+
+  this.placeElements = function() {
+    var documentWidth   = $(document).width();
+    var documentHeight  = $(document).height();
+
+    $editor.width(documentWidth/2);
+    $preview.width(documentWidth/2);
+  };
+
+  (function(){
+    var editor  = ace.edit("editor");
+    var context = this;
+    editor.setTheme("ace/theme/twilight");
+    editor.getSession().setMode("ace/mode/markdown");
+    context.aceEditor = editor;
+
+    $(window).resize(function() {
+      context.placeElements();
+    });
+    context.placeElements();
+  }).call(this);
+
+}
+
+var editor = new Editor();
