d7e2097936eb397e24b632a8a7586095f6c83ef1
Sumeet Singh <ortuna@gmail.com>
2013-09-24 23:40:09 -0500

    Saved file specs

--- app/assets/javascripts/editor/controllers/pendingController.js
+++ app/assets/javascripts/editor/controllers/pendingController.js
@@ -1,9 +1,16 @@
 PendingController = function($scope, $rootScope, changedFileQueue) {
   var context         = this;
   $scope.$element     = $('#pending');
-  $scope.changed = function(){
+  $scope.changed = function() {
     var changedFiles = changedFileQueue.changedFiles();
     return Object.keys(changedFiles);
   };
 
+  $scope.saved = function(path) {
+    changedFileQueue.resetFile(path);
+  };
+
+  $rootScope.$on('saved', function(e, path) {
+    $scope.saved(path);
+  });
 };
--- spec/javascripts/controllers/PendingControllerSpec.js
+++ spec/javascripts/controllers/PendingControllerSpec.js
@@ -8,4 +8,53 @@ describe('controller: PendingController', function() {
     $("#pending").remove();
   });
 
+  beforeEach(inject(function($rootScope, $controller, changedFileQueue) {
+    this.scope  = $rootScope.$new();
+    this.changedFileQueue = changedFileQueue;
+    this.ctrl   = $controller('PendingController', {
+      $scope: this.scope,
+    });
+  }));
+
+  beforeEach(function(){
+    localStorage.clear();
+  });
+
+  describe('#changed', function(){
+    it('retrieves the list of changed files', function(){
+      this.changedFileQueue.fileChanged('a.md');
+      this.changedFileQueue.fileChanged('b.md');
+      expect(this.scope.changed())
+       .toEqual(['a.md', 'b.md']);
+    });
+  });
+
+  describe('#saved', function(){
+    it('removes file from changedFileQueue', function(){
+      this.changedFileQueue.fileChanged('a.md');
+      this.changedFileQueue.fileChanged('b.md');
+
+      this.scope.saved('a.md');
+      expect(this.scope.changed())
+       .toEqual(['b.md']);
+    });
+  });
+
+  describe('Event: saved', function(){
+    it('removes the saved file from the queue', function(){
+      this.changedFileQueue.fileChanged('a.md');
+      this.changedFileQueue.fileChanged('b.md');
+
+      var done = false;
+      this.scope.$on('saved', function(path){ done = true; });
+      this.scope.$emit('saved', 'a.md');
+      
+      waitsFor(function(){ return done; }, 'emit saved', 100);
+      runs(function(){
+        expect(this.scope.changed()).toEqual(['b.md']);
+      });
+      
+    });
+  });
+
 });
