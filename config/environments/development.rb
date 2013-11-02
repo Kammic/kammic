13f7b858b776664fb2b14d140e46dc84f81337f2
Sumeet Singh <ortuna@gmail.com>
2013-11-02 00:47:25 -0400

    ENV keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,10 +8,10 @@ Kammic::Application.configure do
   config.active_record.migration_error = :page_load
   config.assets.debug = true
 
-  config.github_key    = '2e0806ae149248209186'
-  config.github_secret = '84db43fbe23682d7eccb499ea57aebd015fc339e'
+  config.github_key    = ENV['GITHUB_KEY']    || '2e0806ae149248209186'
+  config.github_secret = ENV['GITHUB_SECRET'] || '84db43fbe23682d7eccb499ea57aebd015fc339e'
 
   config.s3_access_key = 'AKIAICK4VG37DHFYCW3A'
   config.s3_secret     = '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'
-  config.s3_bucket     = 'kammic'
+  config.s3_bucket     = 'kammic-dev'
 end
