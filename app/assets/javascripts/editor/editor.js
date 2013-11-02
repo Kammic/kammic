b56fc28822f45aa351a823c27532fef2fd469649
Sumeet Singh <ortuna@gmail.com>
2013-11-02 07:30:53 -0400

    moved editor.js out one dir

--- app/assets/javascripts/editor/editor.js
+++
@@ -1,9 +0,0 @@
-//= require_tree ../lib/bootstrap
-//= require ../lib/angular/angular.min
-//= require ../lib/showdown/compressed/showdown
-//= require ../lib/github/base64
-//= require ../lib/github/underscore
-//= require ../lib/github/octokit
-//= require ../lib/misc/keymaster.min
-
-//= require_tree .
