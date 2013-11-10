1ed91078335634da324542f38229904a3487320f
Sumeet Singh <ortuna@gmail.com>
2013-11-10 00:06:18 -0500

    Fixed specs

--- spec/routing/book_routing_spec.rb
+++ spec/routing/book_routing_spec.rb
@@ -4,18 +4,21 @@ describe 'books controller' do
   it 'can route to the refresh path' do
     expect(get '/books/1/refresh').to route_to(controller: "books",
                                                action:     "refresh",
+                                               format:     :json,
                                                "book_id" =>  "1")
   end
 
   it 'can route to the queue_build path' do
     expect(get '/books/1/queue_build').to route_to(controller: "books",
                                                    action:     "queue",
+                                                   format:     :json,
                                                    "book_id" =>  "1")
   end
 
   it 'can route to the queue_build path' do
     expect(get '/books/1/builds').to route_to(controller: "books",
                                               action: "builds",
+                                              format: :json,
                                               "book_id" =>  "1")
   end
 end
